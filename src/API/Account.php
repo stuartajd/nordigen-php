c95834eab1b00206cbd7db6a507282efc702d151
Stuart Davidson <stuartajd@users.noreply.github.com>
2025-09-12 14:07:42 +0100

    PHP 8.3 fixed dynamic property creation

--- src/API/Account.php
+++ src/API/Account.php
@@ -3,10 +3,15 @@
 namespace Nordigen\NordigenPHP\API;
 
 use Nordigen\NordigenPHP\API\RequestHandler;
+use Nordigen\NordigenPHP\Exceptions\BaseException;
 
 class Account {
 
     private RequestHandler $requestHandler;
+
+    private string $accountId;
+    private int $rateLimit;
+    private int $rateLimitRemaining;
 
     public function __construct(RequestHandler $requestHandler, string $accountId) {
         $this->requestHandler = $requestHandler;
@@ -67,9 +72,21 @@ class Account {
         if($dateFrom) $params['query']['date_from'] = $dateFrom;
         if($dateTo)   $params['query']['date_to']   = $dateTo;
 
-        $response = $this->requestHandler->get("accounts/{$this->accountId}/transactions/", $params);
-        $json = json_decode($response->getBody()->getContents(), true);
-        return $json;
+        try {
+            $response = $this->requestHandler->get("accounts/{$this->accountId}/transactions/", $params);
+            $json = json_decode($response->getBody()->getContents(), true);
+            $exp = null;
+        } catch (BaseException $exception) {
+            $response = $exception->getResponse();
+            $exp = $exception;
+        }
+
+        $this->rateLimit = $response->getHeader("http_x_ratelimit_account_success_limit")[0] ?? 0;
+        $this->rateLimitRemaining = $response->getHeader("http_x_ratelimit_account_success_remaining")[0] ?? 0;
+
+        if ($exp !== null) {
+            throw $exp;
+        }
     }
 
     /**
@@ -93,5 +110,21 @@ class Account {
         $response = $this->requestHandler->get("accounts/premium/{$this->accountId}/transactions/", $params);
         $json = json_decode($response->getBody()->getContents(), true);
         return $json;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getRateLimit(): int
+    {
+        return $this->rateLimit;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getRateLimitRemaining(): int
+    {
+        return $this->rateLimitRemaining;
     }
 }
